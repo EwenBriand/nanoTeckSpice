671641de169cc15f72069c9b3f48651d5c17c032
EwenBriand <ewen.briand@epitech.eu>
2023-02-08 16:54:41 +0100

    [ADD] error handling

--- src/parsing/parse.cpp
+++ src/parsing/parse.cpp
@@ -52,14 +52,11 @@ nts::ControlTower *parse(const char *file)
     } else {
         return nullptr;
     }
-    // std::cout << data;
 
     tokenize(data, '\n', out);
 
     for (auto it = out.begin(); it != out.end(); it++)
         *it = remove_space(*it);
-
-    std::cout << std::endl;
 
     for (auto it = out.begin(); it != out.end();) {
         if (*it == ".chipsets:") {
@@ -68,8 +65,11 @@ nts::ControlTower *parse(const char *file)
         it = out.erase(it);
     }
 
-    for (size_t i = 0; i < out.size(); i++)
-        std::cout << out[i] << std::endl;
+    if (out.size() == 0)
+        return nullptr;
+
+    // for (size_t i = 0; i < out.size(); i++)
+    //     std::cout << out[i] << std::endl;
 
     for (auto it = out.begin(); it != out.end() && stop == 0;) {
         if (*it == ".links:")
@@ -81,6 +81,10 @@ nts::ControlTower *parse(const char *file)
             stop = tower->addElement(line[1], line[0]);
         it = out.erase(it);
     }
+
+    if (out.size() == 0)
+        return nullptr;
+
     for (auto it = out.begin(); it != out.end() && stop == 0; it++) {
         line.clear();
         tokenize(*it, ' ', line);
@@ -89,14 +93,21 @@ nts::ControlTower *parse(const char *file)
             pin2.clear();
             tokenize(line[0], ':', pin1);
             tokenize(line[1], ':', pin2);
-            std::cout << pin1[0] << " : " << tower->getElement(pin1[0])
-                      << std::endl;
-            tower->getElement(pin1[0])->setLink(std::stoi(pin1[1]) - 1,
-                *tower->getElement(pin2[0]), std::stoi(pin2[1]) - 1);
+            if (tower->getElement(pin1[0]) == nullptr) {
+                std::cout << "Unknow component name '" << pin1[0] << "'"
+                          << std::endl;
+                stop = 84;
+            } else if (tower->getElement(pin2[0]) == nullptr) {
+                std::cout << "Unknow component name '" << pin2[0] << "'"
+                          << std::endl;
+                stop = 84;
+            } else
+                tower->getElement(pin1[0])->setLink(std::stoi(pin1[1]) - 1,
+                    *tower->getElement(pin2[0]), std::stoi(pin2[1]) - 1);
         }
     }
 
-    if (stop == 1)
+    if (stop != 0)
         return nullptr;
 
     auto _circuit = tower->getCircuit();
