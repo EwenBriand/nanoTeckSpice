32e830a767c1ab9c42edd3878920d3aa668bd426
EwenBriand <ewen.briand@epitech.eu>
2023-02-20 16:28:02 +0100

    [ADD] no arg

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ int handling_error(nts::ControlTower::Error e, std::string name)
 
 int main(int argc, char **argv, char **env)
 {
-    if (env[0] == NULL)
+    if (env[0] == NULL || argc != 2)
         return ERROR_VALUE;
 
     nts::ControlTower *tower = parse(argv[1]);
@@ -57,55 +57,5 @@ int main(int argc, char **argv, char **env)
     execution(tower);
 
     delete tower;
-    // nts::Output out("output");
-    // nts::Output out2("output2");
-    // nts::Input input(std::string("input1"));
-    // nts::Input input3(std::string("input3"));
-    // nts::Input input2(std::string("input2"));
-    // nts::Output out3("output3");
-    // nts::And and_m;
-    // nts::Xor or_m;
-    // nts::Invert inv;
-
-    // input.setLink(0, and_m, 0);
-    // and_m.setLink(1, input2, 0);
-    // and_m.setLink(2, out, 0);
-
-    // input.getList()[0]->setState(nts::Tristate::False);
-    // input2.getList()[0]->setState(nts::Tristate::Undefined);
-    // and_m.simulate(1);
-    // input.simulate(1);
-    // input2.simulate(1);
-    // out.simulate(1);
-    // out2.simulate(1);
-
-    // or_m.setLink(0, input, 0);
-    // or_m.setLink(1, input2, 0);
-    // or_m.setLink(2, out2, 0);
-
-    // input3.setLink(0, out3, 0);
-
-    // input.getList()[0]->setState(nts::Tristate::False);
-    // input3.getList()[0]->setState(nts::Tristate::False);
-    // input2.getList()[0]->setState(nts::Tristate::True);
-
-    // or_m.simulate(1);
-    // or_m.simulate(1);
-    // or_m.simulate(1);
-    // or_m.simulate(1);
-    // or_m.simulate(1);
-    // input.simulate(1);
-    // input2.simulate(1);
-    // out2.simulate(1);
-
-    // or_m.setLink(2, inv, 0);
-    // inv.setLink(1, out2, 0);
-
-    // inv.simulate(1);
-    // out2.simulate(1);
-    // out3.simulate(1);
-
     return END_VALUE;
 }
-
-// créée les input en tout premier lors du scraping
